48895ce36da164111bf5a13979080baba885d0b0
RainingComputers <vishnu.vish.shankar@gmail.com>
2020-02-29 15:44:26 +0530

    Updates setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Development Status :: 1 - Planning',
+        'Development Status :: 3 - Alpha',
         'Topic :: Scientific/Engineering :: Artificial Intelligence'
     ],
     keywords='pykitml'
