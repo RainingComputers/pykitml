961da616e1db443178a87de0e7d2b9641d7d6ee2
RainingComputers <vishnu.vish.shankar@gmail.com>
2025-01-01 01:43:04 +0530

    Update example for Deep Q learning

--- tests/test_cartpole_dqn.py
+++ tests/test_cartpole_dqn.py
@@ -2,29 +2,31 @@ from pykitml.testing import pktest_graph
 
 @pktest_graph
 def test_cartpole():
-    import numpy as np
-    import gym
+    import gymnasium as gym
     import pykitml as pk
 
     # Wrapper class around the environment
     class Environment:
         def __init__(self):
-            self._env = gym.make('CartPole-v1')
+            self._env = gym.make('CartPole-v1', render_mode="human")
 
         def reset(self):
-            return self._env.reset()
+            return self._env.reset()[0]
 
         def step(self, action):
-            obs, reward, done, _ = self._env.step(action)
+            obs, reward, done, _, _ = self._env.step(action)
+
+            x, _, theta, _ = obs
+            x_threshold = self._env.env.env.env.x_threshold
+            theta_threshold_radians = self._env.env.env.env.theta_threshold_radians
 
             # Reward function, from
-            # https://github.com/keon/deep-q-learning/blob/master/ddqn.py
-            x, _, theta, _ = obs
-            r1 = (self._env.x_threshold - abs(x)) / self._env.x_threshold - 0.8
-            r2 = (self._env.theta_threshold_radians - abs(theta)) / self._env.theta_threshold_radians - 0.5
+            # https://github.com/keon/deep-q-learning/blob/master/ddqn.py            
+            r1 = (x_threshold - abs(x)) / x_threshold - 0.8
+            r2 = (theta_threshold_radians - abs(theta)) / theta_threshold_radians - 0.5
             reward = r1 + r2
 
-            return np.array(obs), reward, done
+            return obs, reward, done
 
         def close(self):
             self._env.close()
